cc6107ca052a80fc367a17855eb17ca4bee0b0f7
hyooo1017 <hyooo1017@gmail.com>
2019-09-10 10:24:49 +0900

    기본 설정

--- Introduction to Machine Learning with Python/basic.py
+++ Introduction to Machine Learning with Python/basic.py
@@ -1,11 +1,32 @@
-from IPython.display import display
+from IPython.display import set_matplotlib_formats, display
 import numpy as np
 import pandas as pd
 import mglearn
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rc, font_manager
+from cycler import cycler
 
 font_name = font_manager.FontProperties(fname = "C:/Windows/Fonts/H2GTRM.TTF").get_name()
 rc('font', family = font_name)
 matplotlib.rcParams['axes.unicode_minus'] = False
+
+set_matplotlib_formats('pdf', 'png')
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['image.cmap'] = "viridis"
+plt.rcParams['image.interpolation'] = "none"
+plt.rcParams['savefig.bbox'] = "tight"
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['legend.numpoints'] = 1
+plt.rc('axes', prop_cycle=(
+    cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
+    cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
+
+
+np.set_printoptions(precision=3, suppress=True)
+
+pd.set_option("display.max_columns", 8)
+pd.set_option('precision', 2)
+
+__all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
