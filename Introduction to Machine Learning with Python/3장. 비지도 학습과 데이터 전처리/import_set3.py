83c9db0017d9b7e4cd9a8c4b458a825dc8e79739
hyooo1017 <hyooo1017@gmail.com>
2019-09-16 22:56:01 +0900

    chapter 3

---
+++ Introduction to Machine Learning with Python/3장. 비지도 학습과 데이터 전처리/import_set3.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import make_blobs
+
+
+from sklearn.model_selection import train_test_split
+
+
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import QuantileTransformer, StandardScaler, PowerTransformer
