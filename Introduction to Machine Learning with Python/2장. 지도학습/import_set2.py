d908486ddf704b7bec6b10b626788763a38ae144
hyooo1017 <hyooo1017@gmail.com>
2019-09-17 21:54:36 +0900

    chapter 2 import 설정

---
+++ Introduction to Machine Learning with Python/2장. 지도학습/import_set2.py
@@ -0,0 +1,31 @@
+# sklearn dataset
+from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_boston
+from sklearn.datasets import make_blobs
+from sklearn.datasets import make_moons
+from sklearn.datasets import make_circles
+from sklearn.datasets import load_iris
+
+from sklearn.model_selection import train_test_split
+import graphviz
+import os
+from mpl_toolkits.mplot3d import Axes3D, axes3d
+
+# sklearn에 있는 모델들
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import export_graphviz
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
