b41b1bd6c0b960c4222e0309be36fe53779c4695
hyooo1017 <hyooo1017@gmail.com>
2019-09-17 22:11:55 +0900

    All chapter import set

--- Introduction to Machine Learning with Python/import_set.py
+++ Introduction to Machine Learning with Python/import_set.py
@@ -5,14 +5,16 @@ from sklearn.datasets import make_blobs
 from sklearn.datasets import make_moons
 from sklearn.datasets import make_circles
 from sklearn.datasets import load_iris
+from sklearn.datasets import fetch_lfw_people
+from sklearn.datasets import load_digits
 
 from sklearn.model_selection import train_test_split
-from sklearn.tree import export_graphviz
+from sklearn.metrics import accuracy_score
 import graphviz
 import os
 from mpl_toolkits.mplot3d import Axes3D, axes3d
 
-# sklearn에 있는 모델들
+# sklearn 모델
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.linear_model import LinearRegression
@@ -22,6 +24,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import export_graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import BaggingClassifier
@@ -30,6 +33,21 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 
-# sklearn에 있는 scaler
+# 전처리
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import QuantileTransformer, StandardScaler, PowerTransformer
+
+# 성분 분석
+from sklearn.decomposition import PCA
+from sklearn.decomposition import NMF
+
+# 매니폴드
+from sklearn.manifold import TSNE
+
+# 클러스터
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
+from scipy.cluster.hierarchy import dendrogram, ward
+from sklearn.metrics.cluster import adjusted_rand_score
+from sklearn.metrics.cluster import silhouette_score
